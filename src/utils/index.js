3f6b457c73e23bb0db9356381a7a2f7b480ad424
emoface1337 <chvrches@mail.ru>
2020-09-14 23:54:40 +0300

    compose function simplify

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,8 @@
-const compose = (...funcs) => (comp) => {
-    return funcs.reduceRight(
-        (wrapped, f) => f(wrapped), comp)
-}
+// const compose = (...funcs) => (comp) => {
+//     return funcs.reduceRight(
+//         (wrapped, f) => f(wrapped), comp)
+// }
+
+const compose = (...funcs) => x => funcs.reduceRight((value,func) => func(value), x)
 
 export default compose
